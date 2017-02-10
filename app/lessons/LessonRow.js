e46c87006dfc8c94af97fe897ce5b541484d2f3b
ijones16 <ian@egghead.io>
2017-02-09 17:45:51 -0800

    Get initial lesson rows rendering

--- app/lessons/LessonRow.js
+++ app/lessons/LessonRow.js
@@ -3,13 +3,26 @@ import { View, Text, StyleSheet } from "react-native";
 
 class LessonRow extends Component {
   render() {
-    const {lesson} = this.props;
+    const {title} = this.props;
     return (
-      <View>
-        <Text>{lesson.title}</Text>
+      <View style={styles.container}>
+        <Text style={styles.text}>{title}</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 10,
+    flexDirection: "row",
+    alignItems: "flex-start",
+    justifyContent: "space-between"
+  },
+  text: {
+    fontSize: 24,
+    color: "#4d4d4d"
+  }
+})
 
 export default LessonRow;
--- app/lessons/Lessons.js
+++ app/lessons/Lessons.js
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, ListView, Platform } from "react-native";
 import LessonRow from "./LessonRow";
 
 class Lessons extends Component {
   constructor(props){
     super(props);
-    state = {
-      lessons: [
-        {
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      lessons: [],
+      dataSource: ds.cloneWithRows([])
+    }
+  }
+
+  setSource = (lessons, itemsDatasource, otherState = {}) => {
+    this.setState({
+      lessons,
+      dataSource: this.state.dataSource.cloneWithRows(itemsDatasource),
+      ...otherState
+    })
+  }
+
+  componentDidMount(){
+    const items = [{
           id: '1',
           title: 'Lesson1'
         },
@@ -22,28 +36,52 @@ class Lessons extends Component {
         {
           id: '4',
           title: 'Lesson4'
-        }
-      ]
-    }
+    }]
+
+    this.setSource(items, items)
   }
 
   render() {
-    const lessonRows = (
-      this.state.lessons.map((lesson) => {
-        <LessonsRow
-          lesson
-          id={lesson.id}
-        />
-      })
-    )
     return (
-      <View>
-        <Text>
-          {lessonRows}
-        </Text>
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <ListView
+            enableEmptySections
+            style={styles.list}
+            dataSource={this.state.dataSource}
+            renderRow={({id, ...value}) => {
+              return (
+                <LessonRow
+                  key={id}
+                  {...value}
+                />
+              )
+            }}
+          />
+        </View>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    ...Platform.select({
+      ios: { paddingTop: 30 }
+    })
+  },
+  content: {
+    flex: 1
+  },
+  list: {
+    backgroundColor: '#FFF'
+  },
+  separator: {
+    borderWidth: 1,
+    borderColor: "#F5F5F5"
+  }
+})
 
 export default Lessons;
