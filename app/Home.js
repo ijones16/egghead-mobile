1c70c1b41fcee5ec5190c1aa6109dde43806d810
Zac Jones <zacjones93@gmail.com>
2017-02-09 17:58:06 -0800

    basic http call working

--- app/Home.js
+++ app/Home.js
@@ -1,12 +1,36 @@
 import React, { Component } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import Lessons from "./lessons/Lessons"
+import  _Environment  from  "../environment"
+import ApiUtils from "./apiUtils/ApiUtils"
+console.log("url: ", _Environment.production.MOBILE_APP_EGGHEAD_BASE_URL)
+const headers = new Headers({
+  "Authorization": `Bearer ${_Environment.production.MOBILE_APP_EGGHEAD_JWT}`,
+  "Content-Type": "application/json"
+}) 
+
+
 
 class Home extends Component {
+  componentDidMount(){
+    this.loadLessons(1);
+  }
+
+  loadLessons = (page) => {
+    return fetch(`${_Environment.production.MOBILE_APP_EGGHEAD_BASE_URL}/lessons?size=5&page=${page}`, {
+        headers
+    }).then(ApiUtils.checkStatus)
+      .then((res) => {
+        res.json().then((lessons) => {
+        console.log(lessons)
+      })
+    })  
+  }
+
   render() {
     return (
       <View>
-        <Lessons />
+        <Text>heyyyyyy</Text>
        </View>
     );
   }
